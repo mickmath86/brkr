bd0d2d25066a4341a908d325dac2f93ad504fa13
mickmath86 <32375999+mickmath86@users.noreply.github.com>
2018-05-11 18:58:22 -0700

    First Commit
    First commit of original files being uploaded to git for brkr app

---
+++ js/progress-bar.js
@@ -0,0 +1,6 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"></script>
+<script src="js/jQuery-plugin-progressbar.js"></script>
+<script>
+$(".progress-bar1").loading();
+
+</script>
